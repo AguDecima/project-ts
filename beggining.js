831d65195a6d031e1961093d045ec8b523822be0
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-03-14 22:23:58 -0300

    primer commit

---
+++ beggining.js
@@ -0,0 +1,28 @@
+// variables
+let a = "2a";
+
+// const
+const b = 3
+
+// functions
+function add(fist, second) {
+    return fist + second
+}
+
+// print
+console.log(add(a,b));
+
+
+// git //
+
+//repository: la carpeta de mi proyecto
+
+// branch: ubicacion donde puedo trabajar
+
+// commit: comentario de mi trabajo
+
+// push: subir lo que trabaje
+
+// pull: traigo las cosas nuevas
+
+// merge: es la forma de fusionar las branch
