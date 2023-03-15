7e94c3a90e28725bd65dacafc60bd9d1d0098d17
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-03-14 22:27:22 -0300

    migrando a otro archivo

--- beggining.js
+++ beggining.js
@@ -11,18 +11,3 @@ function add(fist, second) {
 
 // print
 console.log(add(a,b));
-
-
-// git //
-
-//repository: la carpeta de mi proyecto
-
-// branch: ubicacion donde puedo trabajar
-
-// commit: comentario de mi trabajo
-
-// push: subir lo que trabaje
-
-// pull: traigo las cosas nuevas
-
-// merge: es la forma de fusionar las branch
