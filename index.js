e2a0b11b660257f342a74e9c040b20ba1fd731e8
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-03-17 17:55:17 -0300

    configurando typescript

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const uuid = require('uuid');
-
-console.log({
-    id: uuid.v4(),
-    name: 'Javier',
-    apellido: 'Barboza'
-});
+var num = 2;
+var word = "hello";
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
