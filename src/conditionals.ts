056512db734ca5100a97b2727568b3ed043ccd89
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-03-21 22:54:44 -0300

    condicionales y bucles

---
+++ src/conditionals.ts
@@ -0,0 +1,33 @@
+const age: number = 19
+const country: string = 'arg'
+
+// classic version
+if (age > 18) {
+    console.log('mayor de edad');
+} else {
+    console.log('menor de edad');
+}
+
+// new version
+if (age > 18) console.log('mayor de edad');
+else console.log('menor de edad');
+
+if (age > 18 && country === 'arg') {
+    console.log('pasa la frontera');
+} else if (age > 18 && country === "par") {
+    console.log('vuelva a paraguay');
+} else {
+    console.log('sos de otro lado');
+}
+
+switch (age) {
+    case 17:
+        console.log('menor de edad');
+        break;
+    case 18:
+        console.log('mayor de edad');
+        break;
+    default:
+        console.log('no cumple con requisitos');
+        break;
+}
