ccaa2f641156ab89253d990e9c907fb0fde6426e
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-03-21 23:18:27 -0300

    task

---
+++ src/task.ts
@@ -0,0 +1,13 @@
+type Dev = {
+    user: string;
+    skills: string[];
+}
+
+const devs: Dev[] = require('./task.json');
+const skill = ""
+
+// devuelvan los mejores candidatos que se adapten a mi busqueda
+
+devs.forEach(dev => {
+    
+})
