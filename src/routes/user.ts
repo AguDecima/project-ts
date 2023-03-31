47e5df3d7b8fc6525a09d9f6c85ce565ec32732b
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-03-31 00:04:33 -0300

    task

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,5 +4,6 @@ import { deleteUser, getUser, createUser } from '../controller/user';
 export const router = Router();
 
 router.get('/user', getUser);
+// create GET /user/:id
 router.post('/user', createUser);
 router.get('/delete/:id', deleteUser);
