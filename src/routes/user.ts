0b9db353b5ea8ca7e1ffdf883fc2340bca42f272
Agustin Decima <agustindecima@AguDecimaMac.local>
2023-04-11 22:35:14 -0300

    learning axios to consume http

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { deleteUser, getUser, createUser } from '../controller/user';
+import { getPokemon } from '../controller/pokemon';
 
 export const router = Router();
 
 router.get('/user', getUser);
 // create GET /user/:id
 router.post('/user', createUser);
-router.get('/delete/:id', deleteUser);
+router.delete('/delete/:id', deleteUser);
+router.get('/pokemon', getPokemon);
