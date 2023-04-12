109ae20cf2f88fa0094a3ff977fbcb2cff0dfe16
rafael decima <rafade66@gmail.com>
2023-04-11 22:53:36 -0300

    agregado

---
+++ build/Plus.js
@@ -0,0 +1,7 @@
+"use strict";
+const Plus = require(' ./Plus.json');
+const Data;
+Plus.forEach(Plu => {
+    if (Plu === Datas)
+        console.log(Plu);
+});
---
+++ src/Plus.ts
@@ -0,0 +1,15 @@
+type Plu = {
+    Plu: string;
+    Datas: string[];
+
+}
+
+const Plus: Plu[] = require(' ./Plus.json');
+const Data: "perecedero";
+
+Plus.forEach( Plu => {
+    if(Plu === Datas) console.log(Plu);
+    
+})
+
+
